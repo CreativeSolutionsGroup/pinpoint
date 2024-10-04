b726c4c7afeacd319ebdbe749f1f3e8dda85335b
Zaine F <48459250+ZGeek03@users.noreply.github.com>
2024-10-04 13:38:50 -0400

    Integrated colors

--- theme.ts
+++ theme.ts
@@ -3,22 +3,23 @@ import { createTheme, ThemeOptions } from "@mui/material/styles";
 /*
 Potential Color Choices:
 Earth Yellow:   #D19E4D
-Sea Grean:      #558B62
-Taupe:          #423C38
+Sea Green:      #558B62 **
+  - Dark Sea Green: #3E6044
+Taupe:          #423C38 **
 Midnight Green: #084A5E
 */
 
 export const themeOptions: ThemeOptions = {
   palette: {
-    mode: "light",
+    mode: 'light',
     primary: {
-      main: "#558B62",
+      main: '#3e6044',
     },
     secondary: {
-      main: "#423C38",
+      main: '#d19e4d',
     },
   },
-  typography: {},
+  typography: { },
   components: {
     MuiInputLabel: {
       styleOverrides: {
