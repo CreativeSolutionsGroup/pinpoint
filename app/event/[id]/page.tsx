6f271fafa8b3c55481df4ca4e48f82b48fa5af14
Ashlyn DeVries <ashjdevries@gmail.com>
2024-10-11 16:28:18 -0400

    Revert changes

--- app/event/[id]/page.tsx
+++ app/event/[id]/page.tsx
@@ -1,21 +1,11 @@
-import React from "react";
 import { prisma } from "@/lib/api/db";
 import { redirect } from "next/navigation";
-import { ReactFlow, Controls, MiniMap } from "@xyflow/react";
-import Legend from "@components/Legend";
-import "@xyflow/react/dist/style.css";
 
 export default async function EventMainPage({
   params,
 }: {
   params: { id: string };
 }) {
-  const initialNodes = [
-    { id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
-    { id: "2", position: { x: 0, y: 100 }, data: { label: "2" } },
-  ];
-  const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
-
   const event = await prisma.event.findFirst({
     where: {
       id: params.id,
@@ -27,12 +17,10 @@ export default async function EventMainPage({
   }
 
   return (
-    <div style={{ width: "100vw", height: "100vh" }}>
-      <ReactFlow nodes={initialNodes} edges={initialEdges}>
-        <Controls position="bottom-right" />
-        <MiniMap position="bottom-left" pannable zoomable />
-        <Legend />
-      </ReactFlow>
+    <div className="flex flex-col h-screen">
+      <h1 className="animate-spin text-2xl text-center my-auto">
+        {event.name}
+      </h1>
     </div>
   );
 }
