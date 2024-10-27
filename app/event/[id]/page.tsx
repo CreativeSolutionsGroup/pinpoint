4c2348ed406e6ed22fdd3c3fa93a09e327034fef
Chase Evans <chaseevans@cedarville.edu>
2024-10-27 15:41:45 -0400

    Made the background a map and took away zooming features since only the nodes moved when we zoomed.

--- app/event/[id]/page.tsx
+++ app/event/[id]/page.tsx
@@ -1,22 +1,20 @@
-import React from "react";
 import { prisma } from "@/lib/api/db";
-import { redirect } from "next/navigation";
-import { ReactFlow, Controls, MiniMap } from "@xyflow/react";
 import Legend from "@components/Legend";
+import { Controls, MiniMap, ReactFlow } from "@xyflow/react";
 import "@xyflow/react/dist/style.css";
-
+import { redirect } from "next/navigation";
 
 export default async function EventMainPage({
   params,
 }: {
   params: { id: string };
 }) {
+
   const initialNodes = [
     { id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
     { id: "2", position: { x: 0, y: 100 }, data: { label: "2" } },
   ];
   const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
-
 
   const event = await prisma.event.findFirst({
     where: {
@@ -24,17 +22,33 @@ export default async function EventMainPage({
     },
   });
 
-
   if (!event) {
     redirect("/home?error=Event not found");
   }
 
 
   return (
-    <div style={{ width: "100vw", height: "100vh" }}>
-      <ReactFlow nodes={initialNodes} edges={initialEdges}>
-        <Controls position="bottom-right" />
-        <MiniMap position="bottom-left" pannable zoomable />
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        backgroundColor: "black", // Switch with a theme color**
+        backgroundImage: `url(/Campus.png)`,
+        backgroundSize: "contain",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+      }}
+    >
+      <ReactFlow
+        nodes={initialNodes}
+        edges={initialEdges}
+        zoomOnScroll={false}
+        panOnScroll={false}
+        panOnDrag={false}
+        fitView={true}
+      >
+        <Controls position="bottom-right" showZoom={false}/>
+        <MiniMap position="bottom-left" />
         <Legend />
       </ReactFlow>
     </div>
