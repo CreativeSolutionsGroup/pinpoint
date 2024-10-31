0650c2e176f746b86e1172a35b2ad8f4c90f56d4
Isaac Lloyd <57055268+theisaaclloyd@users.noreply.github.com>
2024-10-31 17:41:59 -0400

    this commit will go down in the git history as the most legendary commit in pinpoint
    we made a legend. also the icons are drag and drop now. and you can copy and paste. you're welcome. - ethan
    
    Co-authored-by: EthanBurdett <EthanBurdett@users.noreply.github.com>

--- app/event/[id]/page.tsx
+++ app/event/[id]/page.tsx
@@ -1,42 +1,22 @@
-import React from "react";
+// EventMainPage.tsx
 import { prisma } from "@/lib/api/db";
 import { redirect } from "next/navigation";
-import { ReactFlow, Controls, MiniMap } from "@xyflow/react";
-import Legend from "@components/Legend";
-import "@xyflow/react/dist/style.css";
-
+import EventFlow from "./EventFlow";  // Adjust the import path as needed
 
 export default async function EventMainPage({
   params,
 }: {
   params: { id: string };
 }) {
-  const initialNodes = [
-    { id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
-    { id: "2", position: { x: 0, y: 100 }, data: { label: "2" } },
-  ];
-  const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
-
-
   const event = await prisma.event.findFirst({
     where: {
       id: params.id,
     },
   });
 
-
   if (!event) {
     redirect("/home?error=Event not found");
   }
 
-
-  return (
-    <div style={{ width: "100vw", height: "100vh" }}>
-      <ReactFlow nodes={initialNodes} edges={initialEdges}>
-        <Controls position="bottom-right" />
-        <MiniMap position="bottom-left" pannable zoomable />
-        <Legend />
-      </ReactFlow>
-    </div>
-  );
+  return <EventFlow event={event} />;
 }
