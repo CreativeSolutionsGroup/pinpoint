5c41c2dc230f6cab4f74deceae9919206cf3e94d
Isaac Lloyd <57055268+theisaaclloyd@users.noreply.github.com>
2025-02-10 02:25:40 -0500

    middleware could probably do more heavy lifting for the role access control but prisma was not happy running here

--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,55 @@
 import withAuth from "next-auth/middleware";
 import { NextRequest, NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
+/* import { prisma } from "@/lib/api/db";
+
+// is the path an edit mode
+const isEditMode = (pathname: string) => {
+  const parts = pathname.split("/");
+  return parts[2] === "edit"; // /event/edit/...
+};
+
+// does the user have permissions?
+const hasEditPermissions = (role?: string) => {
+  if (!role) return false;
+  const allowedRoles = ["EDITOR", "ADMIN"];
+  return allowedRoles.includes(role);
+}; */
 
 export default withAuth(
   async function middleware(request: NextRequest) {
-    // const session = await getServerSession();
-    // const authUser = await prisma.authorizedUser.findFirst({
-    //   where: {
-    //     email: session?.user?.email ?? "",
-    //   },
-    // });
-    
+    // handle the redirect to home
     if (request.nextUrl.pathname === "/") {
       return NextResponse.redirect(`${request.nextUrl.origin}/home`);
     }
-    // if (request.nextUrl.pathname === `/event/${request.nextUrl.pathname.split("/")[2]}/${request.nextUrl.pathname.split("/")[3]}`) {
-      
 
-    // }
+    // get the users token (given to us from next auth)
+    const token = await getToken({ req: request });
+
+    // if theyre not logged in and they're not trying to sign in, redirect to /signin
+    if (!token?.email && request.nextUrl.pathname !== "/signin") {
+      return NextResponse.redirect(`/signin`); //`${request.nextUrl.origin}
+    }
+
+    // for event routes, check the users permissions
+    //! prisma doesn't like running here? moving this logic to the eventFlow page for now
+    /* if (request.nextUrl.pathname.startsWith("/event/")) {
+      // get users roles from database
+      const authUser = await prisma.authorizedUser.findUnique({
+        where: {
+          email: token?.email ?? "",
+        },
+      });
+
+      // if they're trying to edit, make sure they have edit permission or higher
+      if (isEditMode(request.nextUrl.pathname)) {
+        if (!hasEditPermissions(authUser.role)) {
+          const viewPath = request.nextUrl.pathname.replace("/edit/", "/view/");
+          return NextResponse.redirect(`${request.nextUrl.origin}${viewPath}`);
+        }
+      }
+    } */
+    return NextResponse.next();
   },
   {
     callbacks: {
