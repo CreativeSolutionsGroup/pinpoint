46b707c4a4b2a491229d45b80cf09b5865fe840c
Chase Evans <chaseevans@cedarville.edu>
2025-02-24 10:56:01 -0500

    May not work, I wasn

--- components/IconNode.tsx
+++ components/IconNode.tsx
@@ -1,21 +1,25 @@
 "use client";
 
-import * as Icons from "lucide-react";
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
+import { Textarea } from "@/components/ui/textarea";
+import { CustomNode } from "@/types/CustomNode";
 import { Button } from "@mui/material";
 import { NodeProps, useReactFlow } from "@xyflow/react";
-import { CustomNode } from "@/types/CustomNode";
-import ColorMenu from "./ColorMenu";
+import * as Icons from "lucide-react";
 import { useCallback } from "react";
+import ColorMenu from "./ColorMenu";
+
 
 export function IconNode({ data, id }: NodeProps<CustomNode>) {
   const { deleteElements, setNodes } = useReactFlow();
   // Get the icon component from the Lucide icons
   const IconComponent = Icons[data.iconName as keyof typeof Icons.icons];
+
+  
 
   const handleDelete = () => {
     deleteElements({ nodes: [{ id }] });
@@ -50,7 +54,8 @@ export function IconNode({ data, id }: NodeProps<CustomNode>) {
         </PopoverTrigger>
         <PopoverContent className="w-fit">
           <div className="grid gap-4">
-            Place content for the popover here.
+            Notes:
+            <Textarea />
             <Button onClick={handleDelete} color="warning">
               Delete
             </Button>
@@ -60,13 +65,4 @@ export function IconNode({ data, id }: NodeProps<CustomNode>) {
       </Popover>
     </>
   );
-
-  /* return (
-    <div className="bg-white border-2 border-gray-200 rounded-lg p-2 shadow-sm">
-      <div className="flex flex-col items-center gap-1">
-        {IconComponent && <IconComponent className="w-6 h-6 text-gray-700" />}
-        <span className="text-xs text-gray-600">{data.label}</span>
-      </div>
-    </div>
-  ); */
 }
