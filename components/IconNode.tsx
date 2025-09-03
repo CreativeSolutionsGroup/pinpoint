2d663e973db2bee81180f75ad62b9e0edca61405
Ethan Burdett <ethanburdett@cedarville.edu>
2025-09-03 15:03:52 -0400

    made the node settings a dialog instead of a popover on mobile

--- components/IconNode.tsx
+++ components/IconNode.tsx
@@ -6,6 +6,12 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
+import {
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import { Textarea } from "@/components/ui/textarea";
 import { CustomNode } from "@/types/CustomNode";
 import { NextPlan } from "@mui/icons-material";
@@ -245,156 +251,316 @@ export const IconNode = memo(function IconNode({
     );
   }, [data.rotation, id, setNodes]);
 
-  return (
-    <>
-      <Popover onOpenChange={setIsOpen}>
-        <PopoverTrigger
-          style={{
-            borderColor: data.color,
-          }}
-          className="popover-trigger flex flex-col items-center justify-center cursor-move"
-        >
-          {(activeNodeId === id || isOpen) && (
-            <>
-              <div
-                onClick={(e) => {
-                  e.stopPropagation(); // Prevent popover from triggering
-                  handleRotateClockwise();
-                }}
-                style={{
-                  position: "absolute",
-                  top: "50%",
-                  left: "50%",
-                  transform: `translate(-50%, -50%) translate(${
-                    ((data.size == 1 ? 1.5 : data.size) ?? 3) * 15
-                  }px, 0) rotate(90deg)`,
-                  backgroundColor: "rgba(0, 0, 0, 0.8)",
-                  width: "15px",
-                  height: "15px",
-                  borderRadius: "50%",
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center",
-                  cursor: "pointer",
-                  zIndex: 10,
-                }}
-                className="nodrag"
-              >
-                <NextPlan sx={{ color: "white" }} />
-              </div>
-              <div
-                onClick={(e) => {
-                  e.stopPropagation(); // Prevent popover from triggering
-                  handleRotateCounterClockwise();
-                }}
-                style={{
-                  position: "absolute",
-                  top: "50%",
-                  left: "50%",
-                  transform: `translate(-50%, -50%) translate(-${
-                    ((data.size == 1 ? 1.5 : data.size) ?? 3) * 15
-                  }px, 0) scale(-1, 1) rotate(90deg)`,
-                  backgroundColor: "rgba(0, 0, 0, 0.8)",
-                  width: "15px",
-                  height: "15px",
-                  borderRadius: "50%",
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center",
-                  cursor: "pointer",
-                  zIndex: 10,
-                }}
-                className="nodrag"
-              >
-                <NextPlan sx={{ color: "white" }} />
-              </div>
-            </>
-          )}
-          <div
+  // If user is on mobile, make the node settings a dialog instead of a popover
+  const isMobile = /Mobi|Android/i.test(navigator?.userAgent);
+  if (!isMobile) {
+    return (
+      <>
+        <Popover onOpenChange={setIsOpen}>
+          <PopoverTrigger
             style={{
-              transform: `rotate(${Math.round(data.rotation / 45) * 45}deg)`,
+              borderColor: data.color,
             }}
+            className="popover-trigger flex flex-col items-center justify-center cursor-move"
           >
-            <IconComponent
+            {(activeNodeId === id || isOpen) && (
+              <>
+                <div
+                  onClick={(e) => {
+                    e.stopPropagation(); // Prevent popover from triggering
+                    handleRotateClockwise();
+                  }}
+                  style={{
+                    position: "absolute",
+                    top: "50%",
+                    left: "50%",
+                    transform: `translate(-50%, -50%) translate(${
+                      ((data.size == 1 ? 1.5 : data.size) ?? 3) * 15
+                    }px, 0) rotate(90deg)`,
+                    backgroundColor: "rgba(0, 0, 0, 0.8)",
+                    width: "15px",
+                    height: "15px",
+                    borderRadius: "50%",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    cursor: "pointer",
+                    zIndex: 10,
+                  }}
+                  className="nodrag"
+                >
+                  <NextPlan sx={{ color: "white" }} />
+                </div>
+                <div
+                  onClick={(e) => {
+                    e.stopPropagation(); // Prevent popover from triggering
+                    handleRotateCounterClockwise();
+                  }}
+                  style={{
+                    position: "absolute",
+                    top: "50%",
+                    left: "50%",
+                    transform: `translate(-50%, -50%) translate(-${
+                      ((data.size == 1 ? 1.5 : data.size) ?? 3) * 15
+                    }px, 0) scale(-1, 1) rotate(90deg)`,
+                    backgroundColor: "rgba(0, 0, 0, 0.8)",
+                    width: "15px",
+                    height: "15px",
+                    borderRadius: "50%",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    cursor: "pointer",
+                    zIndex: 10,
+                  }}
+                  className="nodrag"
+                >
+                  <NextPlan sx={{ color: "white" }} />
+                </div>
+              </>
+            )}
+            <div
+              style={{
+                transform: `rotate(${Math.round(data.rotation / 45) * 45}deg)`,
+              }}
+            >
+              <IconComponent
+                style={{
+                  color: data.color,
+                  width: `${data.size ?? 3}rem`,
+                  height: `${data.size ?? 3}rem`,
+                }}
+                className="text-gray-700"
+              />
+            </div>
+            <Typography
               style={{
                 color: data.color,
-                width: `${data.size ?? 3}rem`,
-                height: `${data.size ?? 3}rem`,
+                width: "100%",
+                fontSize: `${(data.size ?? 3) / 3}rem`,
+                lineHeight: `${(data.size ?? 3) / 3}rem`,
+                textWrap: "wrap",
+                whiteSpace: "pre-wrap",
+                overflowWrap: "break-word",
+                maxWidth: `${(data.size ?? 3) * 3}rem`,
               }}
-              className="text-gray-700"
-            />
-          </div>
-          <Typography
-            style={{
-              color: data.color,
-              width: "100%",
-              fontSize: `${(data.size ?? 3) / 3}rem`,
-              lineHeight: `${(data.size ?? 3) / 3}rem`,
-              textWrap: "wrap",
-              whiteSpace: "pre-wrap",
-              overflowWrap: "break-word",
-              maxWidth: `${(data.size ?? 3) * 3}rem`,
-            }}
-            className="text-center text-wrap"
-          >
-            {data.label}
-          </Typography>
-        </PopoverTrigger>
-        <PopoverContent className="w-fit">
-          <div className="grid gap-4">
-            {isEditable && (
-              <div className="justify-center">
-                <Input value={data.label} onChange={handleLabelChange} />
-              </div>
-            )}
-            <Textarea
-              placeholder="Add notes"
-              defaultValue={data.notes}
-              onChange={handleNotesChange}
-              disabled={!isEditable}
-            />
-            {isEditable && (
-              <Box className="flex place-content-between">
-                <ResizeMenu
-                  onResize={handleResize}
-                  currentSize={data.size ?? 2}
-                />
-                <Box>
-                  <Tooltip title="Copy Node">
-                    <IconButton
-                      onClick={() => handleCopy()}
-                      sx={{ color: "black" }}
-                    >
-                      <Clipboard />
-                    </IconButton>
-                  </Tooltip>
-                  <Tooltip title="Duplicate Node">
-                    <IconButton
-                      onClick={() => handleDup()}
-                      sx={{ color: "black" }}
-                    >
-                      <CopyPlus />
-                    </IconButton>
-                  </Tooltip>
-                  <Tooltip title="Delete Node">
-                    <IconButton onClick={handleDelete} sx={{ color: "red" }}>
-                      <Trash />
-                    </IconButton>
-                  </Tooltip>
-                </Box>
-              </Box>
-            )}
-            {isEditable && (
-              <ColorMenu
-                x={0}
-                y={0}
-                currentColor={data.color ?? "#000000"}
-                changeColor={colorChange}
+              className="text-center text-wrap"
+            >
+              {data.label}
+            </Typography>
+          </PopoverTrigger>
+          <PopoverContent className="w-fit">
+            <div className="grid gap-4">
+              {isEditable && (
+                <div className="justify-center">
+                  <Input value={data.label} onChange={handleLabelChange} />
+                </div>
+              )}
+              <Textarea
+                placeholder="Add notes"
+                defaultValue={data.notes}
+                onChange={handleNotesChange}
+                disabled={!isEditable}
               />
+              {isEditable && (
+                <Box className="flex place-content-between">
+                  <ResizeMenu
+                    onResize={handleResize}
+                    currentSize={data.size ?? 2}
+                  />
+                  <Box>
+                    <Tooltip title="Copy Node">
+                      <IconButton
+                        onClick={() => handleCopy()}
+                        sx={{ color: "black" }}
+                      >
+                        <Clipboard />
+                      </IconButton>
+                    </Tooltip>
+                    <Tooltip title="Duplicate Node">
+                      <IconButton
+                        onClick={() => handleDup()}
+                        sx={{ color: "black" }}
+                      >
+                        <CopyPlus />
+                      </IconButton>
+                    </Tooltip>
+                    <Tooltip title="Delete Node">
+                      <IconButton onClick={handleDelete} sx={{ color: "red" }}>
+                        <Trash />
+                      </IconButton>
+                    </Tooltip>
+                  </Box>
+                </Box>
+              )}
+              {isEditable && (
+                <ColorMenu
+                  x={0}
+                  y={0}
+                  currentColor={data.color ?? "#000000"}
+                  changeColor={colorChange}
+                />
+              )}
+            </div>
+          </PopoverContent>
+        </Popover>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <Dialog open={isOpen} onOpenChange={setIsOpen}>
+          <DialogTrigger
+            style={{
+              borderColor: data.color,
+            }}
+            className="popover-trigger flex flex-col items-center justify-center cursor-move"
+          >
+            {(activeNodeId === id || isOpen) && (
+              <>
+                <div
+                  onClick={(e) => {
+                    e.stopPropagation(); // Prevent popover from triggering
+                    handleRotateClockwise();
+                  }}
+                  style={{
+                    position: "absolute",
+                    top: "50%",
+                    left: "50%",
+                    transform: `translate(-50%, -50%) translate(${
+                      ((data.size == 1 ? 1.5 : data.size) ?? 3) * 15
+                    }px, 0) rotate(90deg)`,
+                    backgroundColor: "rgba(0, 0, 0, 0.8)",
+                    width: "15px",
+                    height: "15px",
+                    borderRadius: "50%",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    cursor: "pointer",
+                    zIndex: 10,
+                  }}
+                  className="nodrag"
+                >
+                  <NextPlan sx={{ color: "white" }} />
+                </div>
+                <div
+                  onClick={(e) => {
+                    e.stopPropagation(); // Prevent popover from triggering
+                    handleRotateCounterClockwise();
+                  }}
+                  style={{
+                    position: "absolute",
+                    top: "50%",
+                    left: "50%",
+                    transform: `translate(-50%, -50%) translate(-${
+                      ((data.size == 1 ? 1.5 : data.size) ?? 3) * 15
+                    }px, 0) scale(-1, 1) rotate(90deg)`,
+                    backgroundColor: "rgba(0, 0, 0, 0.8)",
+                    width: "15px",
+                    height: "15px",
+                    borderRadius: "50%",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    cursor: "pointer",
+                    zIndex: 10,
+                  }}
+                  className="nodrag"
+                >
+                  <NextPlan sx={{ color: "white" }} />
+                </div>
+              </>
             )}
-          </div>
-        </PopoverContent>
-      </Popover>
-    </>
-  );
+            <div
+              style={{
+                transform: `rotate(${Math.round(data.rotation / 45) * 45}deg)`,
+              }}
+            >
+              <IconComponent
+                style={{
+                  color: data.color,
+                  width: `${data.size ?? 3}rem`,
+                  height: `${data.size ?? 3}rem`,
+                }}
+                className="text-gray-700"
+              />
+            </div>
+            <Typography
+              style={{
+                color: data.color,
+                width: "100%",
+                fontSize: `${(data.size ?? 3) / 3}rem`,
+                lineHeight: `${(data.size ?? 3) / 3}rem`,
+                textWrap: "wrap",
+                whiteSpace: "pre-wrap",
+                overflowWrap: "break-word",
+                maxWidth: `${(data.size ?? 3) * 3}rem`,
+              }}
+              className="text-center text-wrap"
+            >
+              {data.label}
+            </Typography>
+          </DialogTrigger>
+          <DialogContent className="w-fit">
+            <DialogTitle className="text-lg font-semibold">
+              Icon Settings
+            </DialogTitle>
+            <div className="grid gap-4">
+              {isEditable && (
+                <div className="justify-center">
+                  <Input value={data.label} onChange={handleLabelChange} />
+                </div>
+              )}
+              <Textarea
+                placeholder="Add notes"
+                defaultValue={data.notes}
+                onChange={handleNotesChange}
+                disabled={!isEditable}
+              />
+              {isEditable && (
+                <Box className="flex place-content-between">
+                  <ResizeMenu
+                    onResize={handleResize}
+                    currentSize={data.size ?? 2}
+                  />
+                  <Box>
+                    <Tooltip title="Copy Node">
+                      <IconButton
+                        onClick={() => handleCopy()}
+                        sx={{ color: "black" }}
+                      >
+                        <Clipboard />
+                      </IconButton>
+                    </Tooltip>
+                    <Tooltip title="Duplicate Node">
+                      <IconButton
+                        onClick={() => handleDup()}
+                        sx={{ color: "black" }}
+                      >
+                        <CopyPlus />
+                      </IconButton>
+                    </Tooltip>
+                    <Tooltip title="Delete Node">
+                      <IconButton onClick={handleDelete} sx={{ color: "red" }}>
+                        <Trash />
+                      </IconButton>
+                    </Tooltip>
+                  </Box>
+                </Box>
+              )}
+              {isEditable && (
+                <ColorMenu
+                  x={0}
+                  y={0}
+                  currentColor={data.color ?? "#000000"}
+                  changeColor={colorChange}
+                />
+              )}
+            </div>
+          </DialogContent>
+        </Dialog>
+      </>
+    );
+  }
 });
