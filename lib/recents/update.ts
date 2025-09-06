cdb94511cb632e84f6ffa240218db2150b9a2de7
Alexander Taylor <52684858+awtpi314@users.noreply.github.com>
2025-09-06 11:06:43 -0400

    removed transaction

--- lib/recents/update.ts
+++ lib/recents/update.ts
@@ -4,43 +4,42 @@ export async function updateRecents(
   email: string,
   locationId: string
 ): Promise<void> {
-  await prisma.$transaction(async (tx) => {
-    const currentRecents = await tx.recents.findMany({
-      where: { user: { email } },
-      orderBy: { lastUsed: "desc" },
+  const currentRecents = await prisma.recents.findMany({
+    where: { user: { email } },
+    orderBy: { lastUsed: "desc" },
+  });
+
+  const existingRecent = currentRecents.find(
+    (recent) => recent.locationId === locationId
+  );
+
+  if (existingRecent) {
+    await prisma.recents.update({
+      where: { id: existingRecent.id },
+      data: { lastUsed: new Date() },
+    });
+  } else {
+    const locationExists = await prisma.location.findUnique({
+      where: { id: locationId },
     });
 
-    const existingRecent = currentRecents.find(
-      (recent) => recent.locationId === locationId
-    );
-
-    if (existingRecent) {
-      await tx.recents.update({
-        where: { id: existingRecent.id },
-        data: { lastUsed: new Date() },
-      });
-    } else {
-      const locationExists = await tx.location.findUnique({
-        where: { id: locationId },
-      });
-
-      if (!locationExists) {
-        return;
-      }
-
-      await tx.recents.create({
-        data: {
-          user: { connect: { email } },
-          location: { connect: { id: locationId } },
-          lastUsed: new Date(),
-        },
-      });
-      if (currentRecents.length >= 5) {
-        const oldestRecent = currentRecents[currentRecents.length - 1];
-        await tx.recents.delete({
-          where: { id: oldestRecent.id },
-        });
-      }
+    if (!locationExists) {
+      return;
     }
-  });
+
+    await prisma.recents.create({
+      data: {
+        user: { connect: { email } },
+        location: { connect: { id: locationId } },
+        lastUsed: new Date(),
+      },
+    });
+
+    if (currentRecents.length >= 5) {
+      const oldestRecent = currentRecents[currentRecents.length - 1];
+      await prisma.recents.delete({
+        where: { id: oldestRecent.id },
+      });
+    }
+  }
 }
